a74af0c246851f528933abbd86bcfbd185c612bb
Benon78 <wilbenjamin7@gmail.com>
2025-09-03 15:20:36 +0300

    feat/ add the live link for allowed origins

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectDB()
 await connectCloudinary();
 
 // allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://green-cart-project-frontend.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 
